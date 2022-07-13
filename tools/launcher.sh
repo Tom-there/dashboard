9b6f40007514015cd818686cf1add353a0e0d3d6
ToM_There <tom.schreiner@gmx.de>
2022-07-13 18:17:54 +0200

    launcher testing

--- tools/launcher.sh
+++ tools/launcher.sh
@@ -6,20 +6,20 @@ remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 remote=$(git config branch.$local_branch.remote)
 
 echo "Fetching from $remote..."
-git fetch $remote > /dev/null
+git fetch $remote
 
-if git merge-base --is-ancestor $remote_branch HEAD > /dev/null; then
+if git merge-base --is-ancestor $remote_branch HEAD; then
     echo 'Already up-to-date'
     target/release/dashboard   
     exit 0
 fi
 
-if git merge-base --is-ancestor HEAD $remote_branch > /dev/null; then
+if git merge-base --is-ancestor HEAD $remote_branch; then
     echo 'Fast-forward possible. Merging...'
-    git merge --ff-only --stat $remote_branch  > /dev/null
+    git merge --ff-only --stat $remote_branch
 else
     echo 'Fast-forward not possible. Rebasing...'
-    git rebase --preserve-merges --stat $remote_branch > /dev/null
+    git rebase --preserve-merges --stat $remote_branch
 fi
 
 echo "now building"
