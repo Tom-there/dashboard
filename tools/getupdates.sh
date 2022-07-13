45f90e21ef60ac4c253b59df99565e581c3e1161
Tom-there <tom.schreiner@gmx.de>
2022-07-13 15:13:46 +0200

    zesz

--- tools/getupdates.sh
+++ tools/getupdates.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-pwd
-if [[ 'git status --procelain --untracked-files=no' ]]
-then
-  echo changes upstream
-else	
-  echo no changes upstream
-fi
 
 
-#!/bin/bash
+##pwd
+##if [[ 'git status --procelain --untracked-files=no' ]]
+##then
+##  echo changes upstream
+##else	
+##  echo no changes upstream
+###fi
+
+
 set -e
 
 local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
