5d89f4bf93415a71b856b58a31513aa12235bbc7
ToM_There <tom.schreiner@gmx.de>
2022-07-13 18:08:44 +0200

    launcher testing

--- tools/launcher.sh
+++ tools/launcher.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 var = 0
-
 function run {
      if $var > 0; then
         echo now building
@@ -10,21 +8,17 @@ function run {
      echo 'running'
      target/release/dashboard   
 }
-
 function fetchGit {
     local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
     remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
     remote=$(git config branch.$local_branch.remote)
-
     echo "Fetching from $remote..."
     git fetch $remote > /dev/null
-
     if git merge-base --is-ancestor $remote_branch HEAD; then
         echo 'Already up-to-date'
         run
         exit 0
     fi
-
     if git merge-base --is-ancestor HEAD $remote_branch; then
         echo 'Fast-forward possible. Merging...'
         git merge --ff-only --stat $remote_branch  > /dev/null
@@ -35,8 +29,6 @@ function fetchGit {
     $var = 1
     run
 }
-
 echo "currently in $(pwd)"
 set -e
-
 exit 0
