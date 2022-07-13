975615c0140add88d9e8f28fffb3b72e4bb9272f
ToM_There <tom.schreiner@gmx.de>
2022-07-13 17:55:28 +0200

    launcher testing

--- tools/launcher.sh
+++ tools/launcher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+var = 0
+
 function run {
-    if $# > 0; then
+    if $var > 0; then
         cargo build --release
     fi
     echo 'running'
@@ -12,30 +14,33 @@ function build {
     echo 'building'
 }
 
+function fetchGit {
+    local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+    remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+    remote=$(git config branch.$local_branch.remote)
+
+    echo "Fetching from $remote..."
+    git fetch $remote
+
+    if git merge-base --is-ancestor $remote_branch HEAD; then
+        echo 'Already up-to-date'
+        run
+        exit 0
+    fi
+
+    if git merge-base --is-ancestor HEAD $remote_branch; then
+        echo 'Fast-forward possible. Merging...'
+        git merge --ff-only --stat $remote_branch
+    else
+        echo 'Fast-forward not possible. Rebasing...'
+        git rebase --preserve-merges --stat $remote_branch
+    fi
+    var = 1
+}
+
 echo "currently in $pwd"
 set -e
 
-local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
-remote=$(git config branch.$local_branch.remote)
-
-echo "Fetching from $remote..."
-git fetch $remote
-
-if git merge-base --is-ancestor $remote_branch HEAD; then
-    echo 'Already up-to-date'
-    run
-    exit 0
-fi
-
-if git merge-base --is-ancestor HEAD $remote_branch; then
-    echo 'Fast-forward possible. Merging...'
-    git merge --ff-only --stat $remote_branch
-else
-    echo 'Fast-forward not possible. Rebasing...'
-    git rebase --preserve-merges --stat $remote_branch
-fi
-
-tools/launcher.sh b
+fetchGit
 
 exit 0
