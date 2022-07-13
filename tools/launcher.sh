041d1fc4ce98893a5fed849ff4a531dbf98b5b2b
ToM_There <tom.schreiner@gmx.de>
2022-07-13 18:16:32 +0200

    launcher testing

--- tools/launcher.sh
+++ tools/launcher.sh
@@ -8,13 +8,13 @@ remote=$(git config branch.$local_branch.remote)
 echo "Fetching from $remote..."
 git fetch $remote > /dev/null
 
-if git merge-base --is-ancestor $remote_branch HEAD; then
+if git merge-base --is-ancestor $remote_branch HEAD > /dev/null; then
     echo 'Already up-to-date'
     target/release/dashboard   
     exit 0
 fi
 
-if git merge-base --is-ancestor HEAD $remote_branch; then
+if git merge-base --is-ancestor HEAD $remote_branch > /dev/null; then
     echo 'Fast-forward possible. Merging...'
     git merge --ff-only --stat $remote_branch  > /dev/null
 else
